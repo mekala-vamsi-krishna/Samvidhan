dfbe0468e964a6f45b67789b67043297a97fece1
Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2026-05-12 22:46:38 +0530

    [FEATURE] PreambleDetailView Added

---
+++ Samvidhan/Home/View/PreambleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PreambleDetailView.swift
+//  Samvidhan
+//
+//  Created by Mekala Vamsi Krishna on 5/12/26.
+//
+
+import SwiftUI
+
+struct PreambleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreambleDetailView()
+}
